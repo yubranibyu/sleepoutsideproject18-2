bef22a4746209fef46a5b87737b3cbc8c6e5c0c8
yubrani <yag24001@byui.edu>
2025-11-12 20:07:16 -0300

    lastone commit ass 2

---
+++ src/js/product.js
@@ -0,0 +1,22 @@
+import { setLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
+
+// create data source
+const dataSource = new ProductData("tents");
+
+function addProductToCart(product) {
+  setLocalStorage("so-cart", product);
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+  const product = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(product);
+}
+const productId = getParam('product');
+console.log(dataSource.findProductById(productId));
+// add listener to Add to Cart button
+document
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
+  
